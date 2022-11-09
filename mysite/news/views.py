117939c1b51766b15687c3d438eadd0d3d8642ed
Vu_Nguyen <nguyentuanvu9x@gmail.com>
2022-11-09 21:15:39 +0300

    adding queryset attribute comment

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -11,6 +11,9 @@ class HomeNews(ListView):
     template_name = 'news/home_news_list.html'
     context_object_name = 'news'
     # extra_context = {'title': 'Не Главная'}
+    # queryset = News.objects.select_related('category')
+    # the same as method .select_related('category')
+
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(HomeNews, self).get_context_data(**kwargs)

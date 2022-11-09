ae02810b2d48c64094d6be7ad612accda1562caa
Vu_Nguyen <nguyentuanvu9x@gmail.com>
2022-11-09 20:59:52 +0300

    Added Django Toolbar + optimized SQL queries

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -22,6 +22,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('news.urls')),
+    path('__debug__/', include('debug_toolbar.urls')),
 ]
 
 if settings.DEBUG:

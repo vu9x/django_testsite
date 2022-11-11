325f0c49f47a4341b00fcae0e81080ec0324dfa1
Vu_Nguyen <nguyentuanvu9x@gmail.com>
2022-11-11 10:53:00 +0300

    Paginator added to news and categories

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import ListView, DetailView, CreateView
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.paginator import Paginator
 
 from .models import News, Category
 from .forms import NewsForm
@@ -9,11 +10,19 @@ from .utils import MyMixin
 
 # Create your views here.
 
+def test(request):
+    objects = ['john1', 'paul2', 'george3', 'ringo4', 'ringo5', 'ringo6','ringo7']
+    paginator = Paginator(objects, 2)
+    page_num = request.GET.get('page', 1)
+    page_objects = paginator.get_page(page_num)
+    return render(request, 'news/test.html', {'page_obj': page_objects})
+
 class HomeNews(MyMixin, ListView):
     model = News
     template_name = 'news/home_news_list.html'
     context_object_name = 'news'
     mixin_prop = 'hello world'
+    paginate_by = 2
     # extra_context = {'title': 'Не Главная'}
     # queryset = News.objects.select_related('category')
     # the same as method .select_related('category')
@@ -34,6 +43,7 @@ class NewsByCategory(MyMixin, ListView):
     template_name = 'news/home_news_list.html'
     context_object_name = 'news'
     allow_empty = False
+    paginate_by = 2
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
